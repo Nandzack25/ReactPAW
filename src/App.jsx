a94cad157e1b8186116d5b26cc27dde14faa7a7b
Nandzack25 <nandazackyf@gmail.com>
2025-10-18 18:34:16 +0700

    fix: correct import path case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import EmployeeModal from "./components/EmployeeModal";
 
